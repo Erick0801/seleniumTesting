7d229b5f2960008a10766f9ca28e7403ef9c80c3
Erick Chai <Erick.Chai@blackboard.com>
2016-11-25 18:03:27 +0800

    add webDriver class

---
+++ seleniumTesting/seleniumTesting/WebDriver/WebDriverFactory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace seleniumTesting
+{
+	public class WebDriverFactory
+	{
+		/* Browsers constants */
+		public const String CHROME = "chrome";
+		public const String FIREFOX = "firefox";
+		public const String SAFARI = "safari";
+		public const String INTERNET_EXPLORER = "ie";
+
+		/* Platform constants */
+		public const String MAC = "mac";
+		public const String LINUX = "linux";
+		public const String WINDOWS = "windows";
+
+		public WebDriverFactory()
+		{
+		}
+
+		public static IWebDriver getInstance(String gridHubUrl, Browser browser, String username, String password)
+		{
+			IWebDriver webDriver = null;
+
+			DesiredCapabilities capability = new DesiredCapabilities();
+
+		}
+	}
+}
